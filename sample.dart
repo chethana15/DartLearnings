a96cd08aec0dec8dae6e215bd12ffba313bf9173
Chethana N P <chethana.np@cumulations.com>
2022-06-12 16:41:19 +0530

    https://github.com/chethana15/DartLearnings

--- sample.dart
+++ sample.dart
@@ -1,5 +1,12 @@
 import "dart:io";
 
-void main(){
-  print("Hello, World!!");
+void main()
+{
+  print("Enter your name:");
+  String? name = stdin.readLineSync();
+  print("Hello ${name}!!");
+
+  print("${name}, can you enter a number");
+  int? n = int.parse(stdin.readLineSync()!);
+  print("${name} you entered ${n}.");
 }

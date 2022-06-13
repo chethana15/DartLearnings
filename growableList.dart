f305608df9c1efcd3b1d435b5211624bb12f8cf4
Chethana N P <chethana.np@cumulations.com>
2022-06-13 16:19:42 +0530

    growableList

---
+++ growableList.dart
@@ -0,0 +1,43 @@
+//import "dart:io";
+import 'dart:core';
+
+void main(){
+
+  final growableList = <String>["a", "b", "c", "b", "d"];
+  growableList[0] = "Chethana"; //it replaces the previous index value
+  print(growableList);
+
+  growableList.add("e"); //adds an element at the end of the list
+  //growableList.add("f","g"); -> throws error as only one value can be added with is added at the end of list
+  print(growableList);
+
+  growableList.addAll({"f", "g"});
+  print(growableList);
+
+//prints -1 if value is not present in growableList else prints the indexvalue.
+  print(growableList.indexOf('a'));
+  print(growableList.indexOf('c'));
+
+final lastIndexB = growableList.lastIndexOf('b'); 
+print(lastIndexB);//prints the index value of the required value 'b' which comes first from the end of the list
+
+//remove element from list
+growableList.remove('b');//first 'b' is removed from list
+print(growableList);
+
+growableList.removeLast();
+print(growableList);
+
+growableList.removeAt(4);
+print(growableList);
+
+// growableList.removeWhere((element) => element.length == 3);
+// growableList.join(',');
+// print(growableList);
+
+growableList.insert(3, 'satrun');
+print(growableList);
+  
+growableList.shuffle();
+print(growableList);  
+}

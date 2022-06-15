8717ae1604a1b88692dfeefabca1fb6c850b0a95
Chethana N P <chethana.np@cumulations.com>
2022-06-15 13:59:09 +0530

    customExceptionHandling

---
+++ customException.dart
@@ -0,0 +1,33 @@
+// ignore_for_file: unused_local_variable
+
+import 'dart:io';
+void main(){
+
+print("Enter salary:");
+int? salary = int.parse(stdin.readLineSync()!);
+try{
+int? result = yourSalary(salary);
+print(result);
+} on SatisfyBankTermsError catch (e){
+  print(e.errorMessage());
+}
+
+}
+
+//when custom class is created we have to implement interface of exception
+
+class SatisfyBankTermsError implements Exception {
+  String errorMessage(){
+    return "You can't avail loan";
+  }
+  }
+
+  yourSalary(int n){
+  if(n > 10000){
+    print("You are eligible for loan");
+    // throw new SatisfyBankTermsError();
+  }
+  else{
+    throw new SatisfyBankTermsError();
+  }
+ } 

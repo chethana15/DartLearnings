e566e9c147c63f59344a13d0ea24d1bbe1afee50
Chethana N P <chethana.np@cumulations.com>
2022-06-24 12:01:40 +0530

    built in data types

---
+++ 2_builtin_dataTypes.dart
@@ -0,0 +1,40 @@
+void main(){
+
+//All data types in dart are object. If values are not initialised by default it's null.
+int price1 = 20;
+print(price1);
+
+double price2 = 22.66;
+print(price2);
+
+var price3 = 22;
+print(price3);
+
+var price4 = 37.97;
+print(price4);
+
+double value = 2e3;
+print(value);
+var value2 = 2e4;
+print(value2);
+
+String myName = "Chethana";
+print(myName);
+
+var myName2 = "xyz";
+print(myName2);
+
+int hexaDecimalValue = 0xAAAAAAAA;
+print(hexaDecimalValue);
+
+int hexaDecimalValue2 = 0xAAAAAAAB;
+print(hexaDecimalValue2);
+
+bool resultPass = true;
+print(resultPass);
+
+//if we don't initialise values by default it's assigned to null
+var myAge;
+print(myAge);
+
+}

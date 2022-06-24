f92e23fea7f45d84ec80d1b7b24dbc2168b68cc1
Chethana N P <chethana.np@cumulations.com>
2022-06-24 12:24:53 +0530

    strings and string interpolation

---
+++ 3_string_&_stringInterpolation.dart
@@ -0,0 +1,36 @@
+void main(){
+
+  //literals
+  int price = 10;
+  String name = "Chethana";
+  "Dart";
+  100;
+
+  //ways to define String literals 
+
+  String string1 = "Chethana";
+
+  String string11 = 'Chethana';
+
+  String string2 = "I'm Learning Dart";
+
+  String string3 = 'I\'m Learning Dart ';
+
+  String longString = 
+'Mary had a little lamb,'
+'Its fleece was white as snow, yeah.'
+'Everywhere the child went,'
+'The little lamb was sure to go, yeah.';
+
+print(longString);
+
+//String interpolation
+
+print("My name is $name");
+print("Number of characters in $name is ${name.length}");
+
+
+
+
+
+}
